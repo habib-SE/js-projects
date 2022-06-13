7a880895b470a0f6459cc494d1ce0fd2a225dc1f
habib <hab67603@gmail.com>
2022-06-14 00:51:10 +0500

    change style and change content

--- app.js
+++ app.js
@@ -196,9 +196,17 @@
 // obj.edit(22);
 
                 // Loop
-const car=['honda','toyota','sedan','sports']
-car.forEach(function(car,index,arry){
-        console.log(`${index} ${car}`);
-        console.log(arry);
-}); 
-               
+// const car=['honda','toyota','sedan','sports']
+// car.forEach(function(car,index,arry){
+//         console.log(`${index} ${car}`);
+//         console.log(arry);
+// }); 
+                
+              // change style
+              document.getElementById('task-title').style.background=('#333');
+              
+              document.getElementById('task-title').style.color='#fff';
+              document.getElementById('task-title').style.padding='5pxt';
+
+// change content
+document.getElementById('task-title').textContent='Task-List';              
